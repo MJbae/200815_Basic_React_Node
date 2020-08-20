aeb17dc14fe954e1247528fb5428c5aa3aa436bd
MJbae <bmj5208@gmail.com>
2020-08-20 15:26:26 +0900

    feat: 로그아웃 기능 추가

--- client_side/src/components/views/LandingPage/LandingPage.js
+++ client_side/src/components/views/LandingPage/LandingPage.js
@@ -1,11 +1,21 @@
 import React, { useEffect } from "react";
 import axios from "axios";
-function LandingPage() {
+function LandingPage(props) {
   useEffect(() => {
     axios.get("/api/hello").then((response) => {
       console.log(response);
     });
   }, []);
+
+  const onClickHandler = () => {
+    axios.get("/api/users/logout").then((response) => {
+      if (response.data.success) {
+        props.history.push("/login");
+      } else {
+        alert("로그아웃 실패");
+      }
+    });
+  };
 
   return (
     <div
@@ -19,6 +29,7 @@ function LandingPage() {
       }}
     >
       <h2>시작페이지</h2>
+      <button onClick={onClickHandler}>Log out</button>
     </div>
   );
 }
