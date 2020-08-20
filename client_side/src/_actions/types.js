dae8c749875a24e19d0bc0aab2b99cc562fefed8
MJbae <bmj5208@gmail.com>
2020-08-20 13:05:14 +0900

    chore: 로그인페이지 추가에 따른 주석 추가

--- client_side/src/_actions/types.js
+++ client_side/src/_actions/types.js
@@ -1,1 +1,2 @@
-export const LOGIN_USER = "login_user";
+// 타입을 통합적으로 관리하기 위해 types.js 사용
+export const LOGIN_USER = "LOGIN_USER";
--- client_side/src/_actions/user_actions.js
+++ client_side/src/_actions/user_actions.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import { LOGIN_USER } from "./types";
 
+// Redux 활용 step 2: request(-> server) 코드 생성 후, type과 함께 객체 형태 action 반환(-> reducer)
 export function loginUser(dataToSubmit) {
-  // Redux 활용 step 2: request(-> server) 코드 생성 후, type과 함께 객체 형태 action 반환(-> reducer)
   const request = axios
     .post("/api/users/login", dataToSubmit)
     .then((response) => response.data);
