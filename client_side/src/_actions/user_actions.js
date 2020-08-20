b7cad280201a31a0e697966610d4d553a3c7c559
MJbae <bmj5208@gmail.com>
2020-08-20 12:42:12 +0900

    feat: 로그인 성공 시 시작페이지로 이동

--- client_side/src/_actions/user_actions.js
+++ client_side/src/_actions/user_actions.js
@@ -2,9 +2,11 @@ import axios from "axios";
 import { LOGIN_USER } from "./types";
 
 export function loginUser(dataToSubmit) {
+  // Redux 활용 step 2: request(-> server) 코드 생성 후, type과 함께 객체 형태 action 반환(-> reducer)
   const request = axios
     .post("/api/users/login", dataToSubmit)
     .then((response) => response.data);
+
   return {
     type: LOGIN_USER,
     payload: request,
--- client_side/src/components/views/LoginPage/LoginPage.js
+++ client_side/src/components/views/LoginPage/LoginPage.js
@@ -3,32 +3,38 @@ import Axios from "axios";
 import { useDispatch } from "react-redux";
 import { loginUser } from "../../../_actions/user_actions";
 
-function LoginPage() {
+function LoginPage(props) {
+  // Redux 활용 setup
   const dispatch = useDispatch();
-
   const [Email, setEmail] = useState("");
   const [Password, setPassword] = useState("");
 
   const onEmailHandler = (event) => {
     setEmail(event.currentTarget.value);
   };
-
   const onPasswordHandler = (event) => {
     setPassword(event.currentTarget.value);
   };
 
   const onSubmitHandler = (event) => {
-    // submit의 default 기능(??)을 제거해야 이하 로직을 실행시킬 수 있음
+    // submit의 default 기능(default로 설정된 action)을 제거해야 이하 로직을 실행시킬 수 있음
     event.preventDefault();
-
     let body = {
       email: Email,
       password: Password,
     };
 
-    dispatch(loginUser(body));
-
-    Axios.post("/api/users/login", body).then((response) => {});
+    // Redux 활용 step 1: dispatch로 action(loginUser(body)) 전달
+    dispatch(loginUser(body)).then((response) => {
+      // loginSuccess 메세지 확인 후 시작페이지로 이동
+      if (response.payload.loginSuccess) {
+        props.history.push("/");
+      } else {
+        alert("Error");
+      }
+    });
+    // Redux 활용에 따라 아래 request 코드를 user_actions.js로 이동
+    // Axios.post("/api/users/login", body).then((response) => {});
   };
 
   return (
