648f03096bf1169092bbd9eae4814e2dd2cbee78
MJbae <bmj5208@gmail.com>
2020-09-04 14:49:52 +0900

    chore: todolist-app init

--- client/src/components/views/NavBar/NavBar.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function NavBar() {
-  return <div>Hello React!</div>;
-}
-
-export default NavBar;
--- server/config/key.js
+++
@@ -1,7 +0,0 @@
-// DB key를 가져올 때, 개발환경이 Local일 경우 ./dev에서, Production인 경우 ./prod에서
-
-if (process.env.NODE_ENV === "production") {
-  module.exports = require("./prod");
-} else {
-  module.exports = require("./dev");
-}
