b6463273f6517c5212793eca2bc2a5a3d244423c
MJbae <bmj5208@gmail.com>
2020-09-04 17:33:57 +0900

    fix: removeTodos 에러 개선

--- todolist-app/client/src/App.js
+++ todolist-app/client/src/App.js
@@ -3,14 +3,15 @@ import axios from "axios";
 import "./App.css";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setinputValue] = useState("");
+
   // 첫 화면에서 DB 내 todo-list 출력
   useEffect(() => {
     axios.get("/api/printTodos").then((response) => {
       setTodos(response.data);
     });
-  }, []);
-  const [todos, setTodos] = useState([]);
-  const [inputValue, setinputValue] = useState("");
+  }, [todos]);
 
   const changeHandler = (event) => {
     setinputValue(event.currentTarget.value);
@@ -24,13 +25,20 @@ function App() {
       .then((response) => {
         if (response.data.success) {
           setTodos([...todos, response.data]);
-          console.log(todos);
           setinputValue("");
-          console.log(inputValue);
         } else {
           alert("falied to update todos");
         }
       });
+  };
+
+  const removeHandler = (event) => {
+    event.preventDefault();
+
+    axios.get("/api/removeTodos").then((response) => {
+      setTodos([response.data]);
+      setinputValue("");
+    });
   };
 
   return (
@@ -40,8 +48,16 @@ function App() {
           {todos &&
             todos.map((todo, index) => <li key={index}>{todo.todoTitle}</li>)}
           <form className="inputBox" onSubmit={submitHandler}>
-            <input type="text" placeholder="입력란" onChange={changeHandler} />
+            <input
+              type="text"
+              placeholder="입력란"
+              value={inputValue}
+              onChange={changeHandler}
+            />
             <button type="submit">입력</button>
+          </form>
+          <form className="removeButton" onSubmit={removeHandler}>
+            <button type="submit">모두 삭제</button>
           </form>
         </div>
       </header>
